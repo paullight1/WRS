1ce0efb8a90dc9c50d99ae0c257d924e7cbe07cc
Paul Light <nwosupaul3@gmail.com>
2026-08-21 08:09:13 +0100

    feat: add MFA enrollment endpoint

---
+++ api/auth/mfa/enroll.js
@@ -0,0 +1,10 @@
+import { enrollMfa } from '../../_lib/mfa.js'
+import { assertSameOrigin, functionHandler, json, requireMethod } from '../../_lib/http.js'
+import { requireSession } from '../../_lib/session.js'
+
+export default functionHandler(async (request) => {
+  requireMethod(request, 'POST')
+  assertSameOrigin(request)
+  const resolved = await requireSession(request, { verified: true })
+  return json(await enrollMfa(resolved), 201)
+})
