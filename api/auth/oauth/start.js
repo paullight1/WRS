59b17614fc023cfbb5743d3df3bbf84741003d7a
Paul Light <nwosupaul3@gmail.com>
2026-08-21 08:50:22 +0100

    refactor: import OAuth start helpers from server

--- api/auth/oauth/start.js
+++ api/auth/oauth/start.js
@@ -1,5 +1,5 @@
-import { beginOAuth } from '../../_lib/oauth.js'
-import { appendCookies, assertSameOrigin, functionHandler, json, readJson, requireMethod } from '../../_lib/http.js'
+import { beginOAuth } from '../../../server/oauth.js'
+import { appendCookies, assertSameOrigin, functionHandler, json, readJson, requireMethod } from '../../../server/http.js'
 
 export default functionHandler(async (request) => {
   requireMethod(request, 'POST')
