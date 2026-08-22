cda725a9a154dec40cb59329e05a276c8ce82013
Paul Light <nwosupaul3@gmail.com>
2026-08-22 21:44:29 +0100

    feat(ecosystem): route production ecosystem screens

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,13 @@ import Marketplace from './screens/Marketplace.jsx'
 import Academy from './screens/Academy.jsx'
 import Community from './screens/Community.jsx'
 import Referrals from './screens/Referrals.jsx'
+import RewardsProduction from './screens/RewardsProduction.jsx'
+import EventCodeProduction from './screens/EventCodeProduction.jsx'
+import BoostsProduction from './screens/BoostsProduction.jsx'
+import MarketplaceProduction from './screens/MarketplaceProduction.jsx'
+import AcademyProduction from './screens/AcademyProduction.jsx'
+import CommunityProduction from './screens/CommunityProduction.jsx'
+import ReferralsProduction from './screens/ReferralsProduction.jsx'
 import Notifications from './screens/Notifications.jsx'
 import More from './screens/More.jsx'
 import Profile from './screens/Profile.jsx'
@@ -54,6 +61,13 @@ const kyc = (element) => <ProtectedRoute policy="kyc">{element}</ProtectedRoute>
 const DeployScreen = runtimeConfig.isDemo ? Deploy : DeployProduction
 const DeploymentDetailsScreen = runtimeConfig.isDemo ? DeploymentDetails : DeploymentDetailsProduction
 const ActiveDeploymentScreen = runtimeConfig.isDemo ? ActiveDeployment : ActiveDeploymentProduction
+const RewardsScreen = runtimeConfig.isDemo ? Rewards : RewardsProduction
+const EventCodeScreen = runtimeConfig.isDemo ? EventCode : EventCodeProduction
+const BoostsScreen = runtimeConfig.isDemo ? Boosts : BoostsProduction
+const MarketplaceScreen = runtimeConfig.isDemo ? Marketplace : MarketplaceProduction
+const AcademyScreen = runtimeConfig.isDemo ? Academy : AcademyProduction
+const CommunityScreen = runtimeConfig.isDemo ? Community : CommunityProduction
+const ReferralsScreen = runtimeConfig.isDemo ? Referrals : ReferralsProduction
 
 export default function App() {
   return (
@@ -95,13 +109,13 @@ export default function App() {
             <Route path="/wallet/transactions" element={kyc(<Transactions />)} />
             <Route path="/wallet/data-revenue" element={kyc(<DataRevenue />)} />
 
-            <Route path="/rewards" element={verified(<Rewards />)} />
-            <Route path="/rewards/event-code" element={verified(<EventCode />)} />
-            <Route path="/rewards/boosts" element={verified(<Boosts />)} />
-            <Route path="/marketplace" element={verified(<Marketplace />)} />
-            <Route path="/academy" element={verified(<Academy />)} />
-            <Route path="/community" element={verified(<Community />)} />
-            <Route path="/referrals" element={verified(<Referrals />)} />
+            <Route path="/rewards" element={verified(<RewardsScreen />)} />
+            <Route path="/rewards/event-code" element={verified(<EventCodeScreen />)} />
+            <Route path="/rewards/boosts" element={verified(<BoostsScreen />)} />
+            <Route path="/marketplace" element={verified(<MarketplaceScreen />)} />
+            <Route path="/academy" element={verified(<AcademyScreen />)} />
+            <Route path="/community" element={verified(<CommunityScreen />)} />
+            <Route path="/referrals" element={verified(<ReferralsScreen />)} />
             <Route path="/notifications" element={authenticated(<Notifications />)} />
 
             <Route path="/more" element={authenticated(<More />)} />
