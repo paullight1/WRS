aa7328d5ef8e7d63c35355705356ed099ca5ce44
Paul Light <nwosupaul3@gmail.com>
2026-08-22 21:37:51 +0100

    fix(ecosystem): correct marketplace server imports

--- api/marketplace.js
+++ api/marketplace.js
@@ -1,6 +1,6 @@
-import { appendCookies, functionHandler, json, requireMethod } from './server/http.js'
-import { marketplaceCatalog } from './server/ecosystem.js'
-import { requireSession } from './server/session.js'
+import { appendCookies, functionHandler, json, requireMethod } from '../server/http.js'
+import { marketplaceCatalog } from '../server/ecosystem.js'
+import { requireSession } from '../server/session.js'
 
 export default functionHandler(async (request) => {
   requireMethod(request, 'GET')
